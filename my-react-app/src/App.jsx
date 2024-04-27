ebc320ce467008f5c5af238b9ef70f99fb9c290e
DinhAnh2301 <dinhanh2301@gmail.com>
2024-04-27 17:21:01 -0600

    Button component added. Learned about 3 different CSS styling approaches: Internal, External, and Modules.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
-import Button from './Button.jsx'
+import Button from './Button/Button.jsx'
 
 function App() {
 
@@ -11,6 +11,7 @@ function App() {
             <Header></Header>
             <Card></Card>
             <Food></Food>
+            <Button></Button>
             <Footer></Footer>
         </>
     );
