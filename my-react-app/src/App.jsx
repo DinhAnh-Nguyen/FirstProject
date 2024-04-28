0b3c3f4ed50ec1842e52f6ead58f4853e7cdd947
DinhAnh2301 <dinhanh2301@gmail.com>
2024-04-27 18:20:48 -0600

    Student component added. Learned about props.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
 import Button from './Button/Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
 
@@ -10,6 +11,11 @@ function App() {
         <>
             <Header></Header>
             <Card></Card>
+            <Student name="Daniel" age={19} isStudent={true}></Student>
+            <Student name="Nathan" age={18} isStudent={true}></Student>
+            <Student name="Mark" age={18} isStudent={true}></Student>
+            <Student name="Mr. Thi" age={54} isStudent={false}></Student>
+            <Student></Student>
             <Food></Food>
             <Button></Button>
             <Footer></Footer>
@@ -18,3 +24,14 @@ function App() {
 }
 
 export default App
+
+// New Lesson:
+// 1. props = read-only properties that are shared between components.
+//            A parent component can send data to a child components.
+//            <Component key=value />
+
+// 2. propTypes = a mechanism that ensures that the passed value is of the correct datatype
+//                age: PropTypes.number
+
+// 3. defaultProps = default values for props in case they are not passed from the parent component
+//                   name: "Guest"
